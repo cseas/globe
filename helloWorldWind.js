b2d698a62b68c23ac86d1ec8632494838f677a3b
cseas <contact.abhijeetsingh@gmail.com>
2018-10-20 15:40:39 +0530

    Add atmosphere

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -11,9 +11,10 @@ wwd.addLayer(new WorldWind.BMNGOneImageLayer());
 wwd.addLayer(new WorldWind.BMNGLandsatLayer());
 
 // Compass, coordinates and view controls
-wwd.addLayer(new WorldWind.CompassLayer());
+// wwd.addLayer(new WorldWind.CompassLayer());
 wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
-wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+// wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
+wwd.addLayer(new WorldWind.AtmosphereLayer());
 
 
 
