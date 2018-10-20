141b23b11e0363275c1153371825bc27e0af7dd4
cseas <contact.abhijeetsingh@gmail.com>
2018-10-20 12:43:26 +0530

    Add basic layers

--- helloWorldWind.js
+++ helloWorldWind.js
@@ -1,1 +1,10 @@
 var wwd = new WorldWind.WorldWindow("canvasOne");
+
+// Imagery layers
+wwd.addLayer(new WorldWind.BMNGOneImageLayer());
+wwd.addLayer(new WorldWind.BMNGLandsatLayer());
+
+// Compass, coordinates and view controls
+wwd.addLayer(new WorldWind.CompassLayer());
+wwd.addLayer(new WorldWind.CoordinatesDisplayLayer(wwd));
+wwd.addLayer(new WorldWind.ViewControlsLayer(wwd));
